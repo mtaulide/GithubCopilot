9e09d3cc6698ae628482beefdd7895a9781f9a0c
mtaulide <111796930+mtaulide@users.noreply.github.com>
2024-06-10 04:52:29 +0000

    mis cambios

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('h1').addEventListener('click', () => {
+        alert('Has hecho clic en el encabezado!');
+    });
+});
